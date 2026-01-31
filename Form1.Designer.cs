a874c31c355d3598165b289799b9e0383ccf0d84
TeejayMerks <teejay1983@gmail.com>
2026-01-31 03:08:24 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Simple_Json_Value_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeViewJson;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            splitContainer1 = new SplitContainer();
+            treeViewJson = new TreeView();
+            btnApply = new Button();
+            txtValue = new TextBox();
+            lblValue = new Label();
+            lblPath = new Label();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.FromArgb(32, 32, 32);
+            menuStrip1.ForeColor = Color.LightGray;
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(689, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.BackColor = Color.FromArgb(32, 32, 32);
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem });
+            fileToolStripMenuItem.ForeColor = Color.LightGray;
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.BackColor = Color.FromArgb(45, 45, 48);
+            openToolStripMenuItem.ForeColor = Color.WhiteSmoke;
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(123, 22);
+            openToolStripMenuItem.Text = "&Open...";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.BackColor = Color.FromArgb(45, 45, 48);
+            saveToolStripMenuItem.ForeColor = Color.WhiteSmoke;
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(123, 22);
+            saveToolStripMenuItem.Text = "&Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.BackColor = Color.FromArgb(45, 45, 48);
+            saveAsToolStripMenuItem.ForeColor = Color.WhiteSmoke;
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(123, 22);
+            saveAsToolStripMenuItem.Text = "Save &As...";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.BackColor = Color.FromArgb(37, 37, 38);
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.FromArgb(37, 37, 38);
+            splitContainer1.Panel1.Controls.Add(treeViewJson);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.BackColor = Color.FromArgb(37, 37, 38);
+            splitContainer1.Panel2.Controls.Add(btnApply);
+            splitContainer1.Panel2.Controls.Add(txtValue);
+            splitContainer1.Panel2.Controls.Add(lblValue);
+            splitContainer1.Panel2.Controls.Add(lblPath);
+            splitContainer1.Size = new Size(689, 373);
+            splitContainer1.SplitterDistance = 280;
+            splitContainer1.TabIndex = 1;
+            // 
+            // treeViewJson
+            // 
+            treeViewJson.BackColor = Color.FromArgb(30, 30, 30);
+            treeViewJson.Dock = DockStyle.Fill;
+            treeViewJson.ForeColor = Color.LightGray;
+            treeViewJson.HideSelection = false;
+            treeViewJson.LineColor = Color.FromArgb(60, 60, 60);
+            treeViewJson.Location = new Point(0, 0);
+            treeViewJson.Name = "treeViewJson";
+            treeViewJson.Size = new Size(280, 373);
+            treeViewJson.TabIndex = 0;
+            treeViewJson.AfterSelect += treeViewJson_AfterSelect;
+            // 
+            // btnApply
+            // 
+            btnApply.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnApply.BackColor = Color.FromArgb(45, 45, 48);
+            btnApply.FlatAppearance.BorderColor = Color.FromArgb(70, 70, 70);
+            btnApply.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 60, 60);
+            btnApply.FlatAppearance.MouseOverBackColor = Color.FromArgb(55, 55, 55);
+            btnApply.FlatStyle = FlatStyle.Flat;
+            btnApply.ForeColor = Color.WhiteSmoke;
+            btnApply.Location = new Point(299, 334);
+            btnApply.Name = "btnApply";
+            btnApply.Size = new Size(94, 27);
+            btnApply.TabIndex = 3;
+            btnApply.Text = "Apply";
+            btnApply.UseVisualStyleBackColor = false;
+            btnApply.Click += btnApply_Click;
+            // 
+            // txtValue
+            // 
+            txtValue.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtValue.BackColor = Color.FromArgb(25, 25, 25);
+            txtValue.BorderStyle = BorderStyle.FixedSingle;
+            txtValue.ForeColor = Color.WhiteSmoke;
+            txtValue.Location = new Point(12, 53);
+            txtValue.Multiline = true;
+            txtValue.Name = "txtValue";
+            txtValue.ScrollBars = ScrollBars.Vertical;
+            txtValue.Size = new Size(383, 249);
+            txtValue.TabIndex = 2;
+            // 
+            // lblValue
+            // 
+            lblValue.AutoSize = true;
+            lblValue.ForeColor = Color.LightGray;
+            lblValue.Location = new Point(12, 35);
+            lblValue.Name = "lblValue";
+            lblValue.Size = new Size(38, 15);
+            lblValue.TabIndex = 1;
+            lblValue.Text = "Value:";
+            // 
+            // lblPath
+            // 
+            lblPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblPath.BackColor = Color.Transparent;
+            lblPath.ForeColor = Color.LightGray;
+            lblPath.Location = new Point(12, 9);
+            lblPath.Name = "lblPath";
+            lblPath.Size = new Size(383, 23);
+            lblPath.TabIndex = 0;
+            lblPath.Text = "Select a value node on the left.";
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            openFileDialog1.Title = "Open JSON file";
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            saveFileDialog1.Title = "Save JSON file as";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(37, 37, 38);
+            ClientSize = new Size(689, 397);
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip1);
+            ForeColor = Color.LightGray;
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Simple Json Value Editor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Windows.Forms;
+
+namespace Simple_Json_Value_Editor
+{
+    public partial class Form1 : Form
+    {
+        private JsonNode _rootNode;
+        private string _currentFilePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // FILE → Open
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            try
+            {
+                var jsonText = File.ReadAllText(openFileDialog1.FileName);
+                _rootNode = JsonNode.Parse(jsonText, new JsonNodeOptions
+                {
+                    PropertyNameCaseInsensitive = false
+                });
+
+                _currentFilePath = openFileDialog1.FileName;
+                Text = $"Simple Json Value Editor - {_currentFilePath}";
+
+                PopulateTree();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"Failed to open JSON:\n{ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // FILE → Save
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_rootNode == null)
+                return;
+
+            if (string.IsNullOrWhiteSpace(_currentFilePath))
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+                return;
+            }
+
+            SaveJson(_currentFilePath);
+        }
+
+        // FILE → Save As
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_rootNode == null)
+                return;
+
+            if (!string.IsNullOrWhiteSpace(_currentFilePath))
+            {
+                saveFileDialog1.InitialDirectory = Path.GetDirectoryName(_currentFilePath);
+                saveFileDialog1.FileName = Path.GetFileName(_currentFilePath);
+            }
+
+            if (saveFileDialog1.ShowDialog(this) != DialogResult.OK)
+                return;
+
+            SaveJson(saveFileDialog1.FileName);
+            _currentFilePath = saveFileDialog1.FileName;
+            Text = $"Simple Json Value Editor - {_currentFilePath}";
+        }
+
+        private void SaveJson(string path)
+        {
+            try
+            {
+                var json = _rootNode.ToJsonString(new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+                File.WriteAllText(path, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"Failed to save JSON:\n{ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Build the TreeView from the JsonNode
+        private void PopulateTree()
+        {
+            treeViewJson.BeginUpdate();
+            treeViewJson.Nodes.Clear();
+
+            if (_rootNode != null)
+            {
+                var rootNode = new TreeNode("(root)")
+                {
+                    Tag = new JsonNodeTag(_rootNode, null, null, null)
+                };
+                treeViewJson.Nodes.Add(rootNode);
+                BuildChildren(rootNode, _rootNode);
+                rootNode.Expand();
+            }
+
+            treeViewJson.EndUpdate();
+            lblPath.Text = "Select a value node on the left.";
+            txtValue.Clear();
+        }
+
+        private void BuildChildren(TreeNode treeNode, JsonNode jsonNode)
+        {
+            if (jsonNode is JsonObject obj)
+            {
+                foreach (var kvp in obj)
+                {
+                    var child = new TreeNode(kvp.Key)
+                    {
+                        Tag = new JsonNodeTag(kvp.Value, obj, kvp.Key, null)
+                    };
+                    treeNode.Nodes.Add(child);
+                    if (kvp.Value is JsonObject || kvp.Value is JsonArray)
+                    {
+                        BuildChildren(child, kvp.Value);
+                    }
+                }
+            }
+            else if (jsonNode is JsonArray array)
+            {
+                for (int i = 0; i < array.Count; i++)
+                {
+                    var child = new TreeNode($"[{i}]")
+                    {
+                        Tag = new JsonNodeTag(array[i], array, null, i)
+                    };
+                    treeNode.Nodes.Add(child);
+                    if (array[i] is JsonObject || array[i] is JsonArray)
+                    {
+                        BuildChildren(child, array[i]);
+                    }
+                }
+            }
+        }
+
+        // When user selects a node in the TreeView
+        private void treeViewJson_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node?.Tag is not JsonNodeTag tag || tag.Node == null)
+            {
+                lblPath.Text = "No node selected.";
+                txtValue.Clear();
+                return;
+            }
+
+            lblPath.Text = GetJsonPath(e.Node);
+
+            if (IsValueNode(tag.Node))
+            {
+                txtValue.Enabled = true;
+                btnApply.Enabled = true;
+                txtValue.Text = tag.Node.ToJsonString(new JsonSerializerOptions { WriteIndented = false });
+            }
+            else
+            {
+                txtValue.Enabled = false;
+                btnApply.Enabled = false;
+                txtValue.Clear();
+            }
+        }
+
+        private static bool IsValueNode(JsonNode node)
+        {
+            return node is JsonValue;
+        }
+
+        private string GetJsonPath(TreeNode node)
+        {
+            // Build something like: Container.Containers[0].Item.ItemId
+            var parts = node
+                .FullPath
+                .Split(new[] { treeViewJson.PathSeparator }, StringSplitOptions.None);
+
+            return string.Join(".", parts.Skip(1)); // skip (root)
+        }
+
+        // Apply button: update the JSON node from txtValue
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            var selected = treeViewJson.SelectedNode;
+            if (selected?.Tag is not JsonNodeTag tag || tag.Node == null)
+                return;
+
+            if (!IsValueNode(tag.Node))
+                return;
+
+            try
+            {
+                var raw = txtValue.Text.Trim();
+
+                JsonNode newValueNode;
+
+                bool looksLikeJsonLiteral =
+                    raw.Equals("null", StringComparison.OrdinalIgnoreCase) ||
+                    raw.Equals("true", StringComparison.OrdinalIgnoreCase) ||
+                    raw.Equals("false", StringComparison.OrdinalIgnoreCase) ||
+                    raw.StartsWith("\"") ||
+                    raw.StartsWith("{") ||
+                    raw.StartsWith("[") ||
+                    double.TryParse(raw, out _);
+
+                if (!looksLikeJsonLiteral)
+                {
+                    // Treat as string
+                    raw = JsonSerializer.Serialize(raw);
+                }
+
+                newValueNode = JsonNode.Parse(raw);
+
+                // Replace in parent
+                if (tag.Parent is JsonObject parentObj && tag.PropertyName != null)
+                {
+                    parentObj[tag.PropertyName] = newValueNode;
+                    tag.Node = newValueNode;
+                }
+                else if (tag.Parent is JsonArray parentArray && tag.Index.HasValue)
+                {
+                    parentArray[tag.Index.Value] = newValueNode;
+                    tag.Node = newValueNode;
+                }
+                else if (_rootNode == tag.Node)
+                {
+                    _rootNode = newValueNode;
+                }
+
+                selected.Tag = tag;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, $"Failed to apply value:\n{ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private sealed class JsonNodeTag
+        {
+            public JsonNode Node;
+            public JsonNode? Parent;  // either JsonObject or JsonArray
+            public string? PropertyName;
+            public int? Index;
+
+            public JsonNodeTag(JsonNode node, JsonNode? parent, string? propertyName, int? index)
+            {
+                Node = node;
+                Parent = parent;
+                PropertyName = propertyName;
+                Index = index;
+            }
+        }
+    }
+}
